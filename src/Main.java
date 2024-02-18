aa8b8265873164226cc4d278a8beac7d6c7b94d4
Pedrosk <augpeter99@outloo.com>
2024-02-18 20:54:07 -0300

    changes

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,7 @@ import java.util.Scanner;
 import java.util.Random;
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("GERADOR DE SENHA");
-
-        Random random = new Random();
-
-        System.out.println(random);
-
-
-
+        System.out.println("Hello World!");
 
     }
 }
